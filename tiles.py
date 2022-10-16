a210700c6089ce715b25e07ccafba4b91f6041ad
imanoliri <imanol.irizar13@gmail.com>
2022-10-16 13:57:50 +0200

    automatic tilemap to excel

--- tiles.py
+++ tiles.py
@@ -9,9 +9,9 @@ from PIL import Image
 from typing import Tuple
 import pandas as pd
 from pathlib import Path
+import os
 
 Tile = str
-#TileMap = np.array
 
 
 @dataclass
@@ -24,7 +24,7 @@ class TileMap():
 
     def __post_init__(self):
         if self.tiles is None:
-            self.tiles = self.tiles_from_image()
+            self.tiles = self._tiles_from_image()
 
     @property
     def tile_counts(self) -> pd.DataFrame:
@@ -41,7 +41,7 @@ class TileMap():
 
         return tile_counts
 
-    def tiles_from_image(self) -> np.array:
+    def _tiles_from_image(self) -> np.array:
         image = self.image
         ratio = image.shape[1] / image.shape[0]
         x_tiles = math.floor(math.sqrt(self.tile_number * ratio))
@@ -54,10 +54,10 @@ class TileMap():
             for y in range(y_tiles):
                 img_tile = image[y * y_resol:(y + 1) * y_resol,
                                  x * x_resol:(x + 1) * x_resol]
-                tiles[y, x] = self.img_rgb_2_tile(img_tile)
+                tiles[y, x] = self._img_rgb_2_tile(img_tile)
         return tiles
 
-    def img_rgb_2_tile(self, image: np.array) -> Tile:
+    def _img_rgb_2_tile(self, image: np.array) -> Tile:
         """
         Convert the passed RGB-array to a single tile, by checking its mean color against all possible Tiles.
 
@@ -71,17 +71,17 @@ class TileMap():
         classifications = []
         for x in range(image.shape[0]):
             for y in range(image.shape[1]):
-                classifications.append(self.closest_tile_for_color(image[x,
-                                                                         y]))
+                classifications.append(
+                    self._closest_tile_for_color(image[x, y]))
 
         # Get distribution of classifications
         df_classifications = pd.Series(classifications, name='tile')
         classification_counts = df_classifications.value_counts(normalize=True,
                                                                 sort=True)
         # Get tile type from classification counts
-        return self.tile_from_classification_counts(classification_counts)
+        return self._tile_from_classification_counts(classification_counts)
 
-    def closest_tile_for_color(self, rgb: Tuple[float]) -> Tile:
+    def _closest_tile_for_color(self, rgb: Tuple[float]) -> Tile:
         """
         Return which Tile is the closest to this color.
 
@@ -100,7 +100,7 @@ class TileMap():
         return min(diffs_and_letters)[1]
 
     @staticmethod
-    def tile_from_classification_counts(counts: pd.Series) -> str:
+    def _tile_from_classification_counts(counts: pd.Series) -> str:
         """
         Tile will be either the top classification count.
         If the difference with the second is not so big, the Tile will be both classes at the same time.
@@ -122,18 +122,63 @@ class TileMap():
                 tile = f'{first_class}/{second_class}'
         return tile
 
+    def to_excel(self, fp: str, image_alpha: float = 0.3):
+        sheet_name = 'map'
+        df_tiles = pd.DataFrame(self.tiles)
 
-def image_file_to_tilemap_file(image_filepath: str, tile_info_kwargs: dict,
-                               map_tile_number: int, *args, **kwargs):
+        # Tiles to excel
+        writer = pd.ExcelWriter(fp, engine='xlsxwriter')
+        df_tiles.to_excel(writer,
+                          sheet_name=sheet_name,
+                          index=False,
+                          header=False)
+
+        # Add image with alpha over tiles
+        alpha_image_filepath = f'{fp[:-5]}_alpha.png'
+        image = Image.fromarray(self.image)
+        image_rgba = image.convert("RGBA")
+        image_rgba.putalpha(int(image_alpha * 255))
+        image_rgba.save(alpha_image_filepath)
+        worksheet = writer.sheets[sheet_name]
+        worksheet.insert_image('A1', alpha_image_filepath)
+
+        # Save to excel & remove temporary image
+        writer.save()
+        os.remove(alpha_image_filepath)
+
+
+def image_file_to_tilemap_file(image_filepath: str, map_tile_number: int,
+                               tile_info_kwargs: dict, image_alpha: float,
+                               *args, **kwargs):
     """
     This function reads a target image, which is expected to be a google maps snippet
     and saves it as a TileMap to a csv.
     """
 
     # Load image and tile information
+    image, tile_info = get_image_and_tile_info(image_filepath,
+                                               tile_info_kwargs)
+
+    # Convert image to tilemap (if not yet done)
+    tilemap_filepath = f"{image_filepath.split('.')[0]}.xlsx"
+    tilemap = None
+    if not Path(tilemap_filepath).is_file():
+        tilemap = TileMap(image, map_tile_number, tile_info)
+        tilemap.to_excel(tilemap_filepath, image_alpha)
+
+    # Create and save tile counts to excel (if not yet done)
+    tilemap_tile_counts_filepath = f"{image_filepath.split('.')[0]}_tile_counts.xlsx"
+    if not Path(tilemap_tile_counts_filepath).is_file():
+        if tilemap is None:
+            tilemap = TileMap(image, map_tile_number, tile_info)
+        tilemap.tile_counts.to_excel(tilemap_tile_counts_filepath, index=False)
+
+
+def get_image_and_tile_info(image_filepath: str,
+                            tile_info_kwargs: dict) -> tuple:
     with open(image_filepath, "rb") as fp:
-        img = Image.open(fp)
-        img = np.array(img)
+        image = Image.open(fp)
+        image = np.array(image)
     with open(tile_info_kwargs["filepath"], 'rb') as fp:
         tile_info = pd.read_excel(fp,
                                   tile_info_kwargs["sheetname"],
@@ -142,17 +187,4 @@ def image_file_to_tilemap_file(image_filepath: str, tile_info_kwargs: dict,
             tuple(int(c) for c in color[1:-1].split(','))
             for color in tile_info.color
         ]
-
-    # Convert image to tilemap (if not yet done)
-    tilemap_filepath = f"{image_filepath.split('.')[0]}.xlsx"
-    tilemap = None
-    if not Path(tilemap_filepath).is_file():
-        tilemap = TileMap(img, map_tile_number, tile_info)
-        pd.DataFrame(tilemap.tiles).to_excel(tilemap_filepath, index=False)
-
-    # Create and save tile counts to excel (if not yet done)
-    tilemap_tile_counts_filepath = f"{image_filepath.split('.')[0]}_tile_counts.xlsx"
-    if not Path(tilemap_tile_counts_filepath).is_file():
-        if tilemap is None:
-            tilemap = TileMap(img, map_tile_number, tile_info)
-        tilemap.tile_counts.to_excel(tilemap_tile_counts_filepath, index=False)
+    return image, tile_info
